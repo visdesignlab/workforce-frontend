9aee83ac57960dc82e5116120a5d59f8e080f998
Kaleb Bickmore <kalebbickmore@kalebs-mbp.lan>
2019-08-26 09:41:06 -0600

    added readme and added typescript configuration to webpack #1

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,31 @@
 const path = require('path');
 
-module.exports = {
-	mode: 'development',
-  entry:{'bundle.js': [
-    path.resolve(__dirname, 'src/index.js')
 
-  ]},
+module.exports = {
+  entry: "./src/index.ts",
   output: {
-    filename: '[name]',
-    path: path.resolve(__dirname, 'dist')
-  }
+      filename: "bundle.js",
+      path: path.resolve(__dirname, 'dist')
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+	mode: 'development',
+
+  resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [ ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+
+  module: {
+      rules: [
+          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+          { test: /\.js$/, loader: "source-map-loader" }
+      ]
+  
+        },
+
+  // Other options...
 };
