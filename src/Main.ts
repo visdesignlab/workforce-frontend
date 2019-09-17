198d723e3b076038ac62c842764265872ed434b1
Kaleb Bickmore <kalebbickmore@kalebs-mbp.uconnect.utah.edu>
2019-09-17 10:55:10 -0600

    saving changes to bootstrap/ merge updates

--- src/Main.ts
+++ src/Main.ts
@@ -2,6 +2,6 @@ import {Map} from './Map';
 import {MapEvents} from './MapEvents'
 import * as d3 from 'd3';
 import 'bootstrap';
-
+import 'bootstrap-select';
 let myMap: Map = new Map();
 let myMapEvents:MapEvents = new MapEvents(myMap);
