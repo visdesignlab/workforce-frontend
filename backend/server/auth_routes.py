a5a297a961c5cee0e583bf0b8fd086a428ec9096
Jack Wilburn <jackwilburn@tutanota.com>
2020-08-14 08:56:46 -0600

    fix issue with session doesn't exist

--- backend/server/auth_routes.py
+++ backend/server/auth_routes.py
@@ -56,7 +56,7 @@ def whoami():
 
 @app.route("/api/logout")
 def logout():    
-    utils.delete_session(flask.session["token"])
+    utils.delete_session(flask.session.get("token"))
     flask.session["token"] = None
     return "Logged out"
 
