26ae1903a3211428426cad6f44a47c3d9c1b16ec
Jack Wilburn <jackwilburn@tutanota.com>
2020-08-14 09:02:23 -0600

    fix check_same_thread

--- backend/server/db.py
+++ backend/server/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from server.db_def import Session, Base
 
-engine = create_engine('sqlite:///sessions.db')
+engine = create_engine('sqlite:///sessions.db', connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
