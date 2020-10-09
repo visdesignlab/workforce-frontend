1b35d94844335ecb7d93e1193c328a7182d48ffc
Jack Wilburn <jackwilburn@tutanota.com>
2020-10-09 10:21:39 -0600

    Fix cookie split

--- frontend/src/API_utils.ts
+++ frontend/src/API_utils.ts
@@ -2,7 +2,7 @@ export function getCookie(name: string) {
     var cookieValue = null;
 
     if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
+        var cookies = document.cookie.split('; ');
         for (var i = 0; i < cookies.length; i++) {
             var cookie = cookies[i];
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
